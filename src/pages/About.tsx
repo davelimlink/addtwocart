2909660419d08142e69704460668f98126ea8516
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-11-27 13:29:30 +0900

    about and home section modify

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,22 @@
 export default function About() {
   return (
-    <h2>
-      "Welcome to the store where variety meets convenience. From essentials to
-      extras, we’ve got everything you’re looking for. No need to shop anywhere
-      else—find it all under one roof. Quality products, great prices, and
-      endless options. Your one-stop shop for everything you need, and more!"
-    </h2>
+    <>
+      <div className="d-flex align-items-center justify-content-center flex-column gap-5">
+        <div className="mt-5">
+          <h1>About us:</h1>
+          <h2>
+            "Welcome to the store where variety meets convenience. From
+            essentials to extras, we’ve got everything you’re looking for. No
+            need to shop anywhere else—find it all under one roof. Quality
+            products, great prices, and endless options. Your one-stop shop for
+            everything you need, and more!"
+          </h2>
+        </div>
+      </div>{" "}
+      <div>
+        <h4 className="text-secondary mt-5">Contact us:</h4>
+        <p>email: addtwocart.business@gmail.com</p>
+      </div>
+    </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1>"Everything You Need, All in One Place!"</h1>;
+  return (
+    <>
+      <div className="d-flex align-items-center justify-content-center">
+        <div className="mt-5">
+          <h1>Shop Now</h1>
+          <h2 className="text-muted text-align-center">
+            "Everything You Need, All in One Place!"
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
