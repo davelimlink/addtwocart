56aedbdbcf6af7771d1ea00ec8c90ba65390f820
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-11-27 13:11:57 +0900

    about us page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,10 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <h2>
+      "Welcome to the store where variety meets convenience. From essentials to
+      extras, we’ve got everything you’re looking for. No need to shop anywhere
+      else—find it all under one roof. Quality products, great prices, and
+      endless options. Your one-stop shop for everything you need, and more!"
+    </h2>
+  );
 }
