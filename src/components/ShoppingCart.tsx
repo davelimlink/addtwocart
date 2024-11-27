f5b53b81658c0ba535144f64d8d45d581317d3a3
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-11-27 13:04:31 +0900

    finishing all the codes needed on addtwocart. (forgot to save)

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { Button, Offcanvas, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json";
 
-export default function ShoppingCart() {
-  return <div>ShoppingCart</div>;
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart();
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
+        </Stack>
+        <Button className="w-100 mt-4" style={{}}>
+          Checkout
+        </Button>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 }
